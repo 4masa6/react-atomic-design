099285a1837554fb7c32bb0bbec58e70c52ff925
tarian <70381096+4masa6@users.noreply.github.com>
2022-04-17 23:01:14 +0900

    Pageの作成

--- src/App.js
+++ src/App.js
@@ -1,34 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "テスト",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "test@test.com",
-  phone: "090-1234-5678",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://test.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>あああ</PrimaryButton>
-        <SecondaryButton>あああ</SecondaryButton>
-        <br />
-        <SearchInput />
-        <br />
-        <UserCard user={user}></UserCard>
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
