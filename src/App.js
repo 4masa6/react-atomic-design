ad1125d9cd9c9c6971489a92eb9ee65bf7e6a3ad
tarian <70381096+4masa6@users.noreply.github.com>
2022-04-17 22:41:57 +0900

    Templateの作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
 
 const user = {
@@ -17,13 +20,15 @@ const user = {
 
 export default function App() {
   return (
-    <div className="App">
-      <PrimaryButton>あああ</PrimaryButton>
-      <SecondaryButton>あああ</SecondaryButton>
-      <br />
-      <SearchInput />
-      <br />
-      <UserCard user={user}></UserCard>
-    </div>
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>あああ</PrimaryButton>
+        <SecondaryButton>あああ</SecondaryButton>
+        <br />
+        <SearchInput />
+        <br />
+        <UserCard user={user}></UserCard>
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
