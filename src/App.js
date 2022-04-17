e2575947b0bf75fc78b21f4cad3ab9e6937f92a3
tarian <70381096+4masa6@users.noreply.github.com>
2022-04-17 22:26:04 +0900

    Organismの作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "テスト",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "test@test.com",
+  phone: "090-1234-5678",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://test.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,8 @@ export default function App() {
       <SecondaryButton>あああ</SecondaryButton>
       <br />
       <SearchInput />
+      <br />
+      <UserCard user={user}></UserCard>
     </div>
   );
 }
